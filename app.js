5e8d9a41f2b7c484e4d3184b59a3ba7dabe134c5
Vital Kabantsou <Vitali86@tut.by>
2024-01-08 18:12:27 +0300

    refactor: update HTML according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="button delete";
   deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.alt = 'Remove';
   deleteButtonImg.className="del-img"
   deleteButton.appendChild(deleteButtonImg);
 
